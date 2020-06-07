64e152a4a2863627affe11146e7c212d9056c089
Fredrik Söderström <tirithen@gmail.com>
2020-06-07 23:44:18 +0200

    refactor(assertEqual): improve equal comparison

--- source/dutils/testing.d
+++ source/dutils/testing.d
@@ -1,10 +1,33 @@
 module dutils.testing;
 
+import std.algorithm.comparison : equal;
+import std.traits : isNumeric, isBoolean;
+
 void assertEqual(T)(T actual, T expected) {
-  if (actual != expected) {
+  bool equals = false;
+
+  static if (isNumeric!T || isBoolean!T) {
+    equals = actual == expected;
+  } else {
+    equals = equal(actual, expected);
+  }
+
+  if (!equals) {
     import std.conv : to;
 
-    throw new Exception("Expected " ~ actual.to!string ~ " to equal " ~ expected.to!string);
+    string actualString;
+    string expectedString;
+    try {
+      actualString = actual.to!string;
+      expectedString = expected.to!string;
+    } catch (Exception exception) {
+    }
+
+    if (actualString != "" && expectedString != "") {
+      throw new Exception("Expected " ~ actualString ~ " to equal " ~ expectedString);
+    }
+
+    throw new Exception("Output did not equal expected output");
   }
 }
 
@@ -27,4 +50,55 @@ unittest {
   }
 
   assertEqual(add(12, 4), 16);
+}
+
+unittest {
+  assertEqual("testing", "testing");
+}
+
+unittest {
+  bool failed = false;
+
+  try {
+    assertEqual("bad text", "testing");
+    failed = true;
+  } catch (Exception exception) {
+    assert(exception.message == "Expected bad text to equal testing");
+  }
+
+  assert(failed == false);
+}
+
+unittest {
+  assertEqual(123, 123);
+}
+
+unittest {
+  bool failed = false;
+
+  try {
+    assertEqual(123, 1232);
+    failed = true;
+  } catch (Exception exception) {
+    assert(exception.message == "Expected 123 to equal 1232");
+  }
+
+  assert(failed == false);
+}
+
+unittest {
+  assertEqual([123, 1, 3, 4], [123, 1, 3, 4]);
+}
+
+unittest {
+  bool failed = false;
+
+  try {
+    assertEqual([123, 1, 3], [123, 1, 3, 4]);
+    failed = true;
+  } catch (Exception exception) {
+    assert(exception.message == "Expected [123, 1, 3] to equal [123, 1, 3, 4]");
+  }
+
+  assert(failed == false);
 }
